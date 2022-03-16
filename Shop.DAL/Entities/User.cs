84b87407ee94d45afff94b0f5e9234dec5e26901
Pavel Urusov <pavell.urusov@gmail.com>
2022-03-17 00:41:30 +0300

    created an entity User

--- Shop.DAL/Entities/User.cs
+++ Shop.DAL/Entities/User.cs
@@ -7,6 +7,14 @@ namespace Shop.DAL.Entities
     public class User : IdentityUser, IBaseEntity
     {
         public Guid Id { get; set; }
+        public DateTime CreateDate { get; set; }
+
+        public Country Country { get; set; }
+        public Guid CountryId { get; set; }
+
+        public IEquatable<Item> BagItems { get; set; }
+
+        public IEquatable<Item> FavoriteItems { get; set; }
 
     }
 }
